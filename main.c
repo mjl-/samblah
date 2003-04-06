db977ef92d928391760c76e00546f58f23959f21
mechiel <devnull@localhost>
2003-04-06 12:44:57 +0200

    better error message when $HOME/.smb/smb.conf doesn't exist.

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ main(int argc, char *argv[])
 	(void)setlocale(LC_ALL, "");
 
 	if (!smb_init())
-		errx(1, "could not initialize samba library, make sure you "
-		    "have a $HOME/.smb/smb.conf");
+		errx(1, "could not initialize libsmbclient, make sure you "
+		    "have an (empty) $HOME/.smb/smb.conf");
 
 	/*
 	 * parse options, read configuration files, et cetera.  if it
